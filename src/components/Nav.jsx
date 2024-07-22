ab24d14cb21e92aa7d98c2be5473bcf2a41a380c
Antonio Alvarado <antoniojalvarado53@gmail.com>
2024-07-22 13:10:55 -0400

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+function Nav() {
+  return (
+    <>
+      <nav className="">
+        <div className=""></div>
+        <ul className="" id="nav-links">
+            <li className="nav-link"></li>
+            <li className="nav-link"></li>
+            <li className="nav-link"></li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+  
+export default Nav
